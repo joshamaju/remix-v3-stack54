fa04aa1b203ae28948b05ae548b2aaf2d7c67edb
Joshua Amaju <joshsemail4work@gmail.com>
2025-11-15 18:32:44 +0100

    chore

--- stack.config.js
+++ stack.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "stack54/config";
 import { server } from "./plugins/server.js";
+import { logger } from "./plugins/logger.js";
 
 export default defineConfig({
   staticDir: "public",
-  integrations: [server()],
   views: ["src/views/**/*.svelte"],
+  integrations: [server(), logger()],
 });
